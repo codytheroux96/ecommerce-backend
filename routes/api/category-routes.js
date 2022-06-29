46f950535eb50d5d2f667fb2042122a6b83199c3
cdt96 <cody.theroux3@gmail.com>
2022-06-29 16:53:46 -0400

    fixed my router.delete methods

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,7 +68,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update(req.body,
+  Category.update(req.body, 
     {
       where: {
         id: req.params.id
@@ -87,8 +87,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Category.destroy(req.body,
-    {
+  Category.destroy({
       where: {
         id: req.params.id
       }
